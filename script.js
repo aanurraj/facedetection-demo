b201d0e4334ada9003a520a13c7922341d638d0b
Abhimanyu Dwivedi <abhimanyudwivedi@outlook.com>
2020-09-17 12:47:24 +0530

    revert

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const video = document.getElementById('video')
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('facedetection-demo/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('facedetection-demo/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('facedetection-demo/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('facedetection-demo/models')
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
